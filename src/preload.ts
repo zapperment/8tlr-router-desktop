e6a18e32f9ea8de74c9054710ef3defb89f87611
Patrick Hund <patrick.hund@adevinta.com>
2024-10-28 18:13:35 +0100

    chore: remove comments from template

--- src/preload.ts
+++ src/preload.ts
@@ -1,6 +1,3 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
-
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { contextBridge, ipcRenderer } = require("electron/renderer");
 
